92f9396c6329125a6739f707077137114c1e8d9d
Mateusz Kocz <mateusz@kocz.pl>
2017-03-04 14:03:25 +0100

    Observables

--- src/app/videos.service.ts
+++ src/app/videos.service.ts
@@ -1,30 +1,45 @@
 import {Injectable} from '@angular/core';
 import {Video} from './Video';
+import {Observable} from 'rxjs/Observable';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class VideosService {
-  private items: Video[];
+  private items: BehaviorSubject<Video[]>;
 
-  get videos(): Video[] {
+  get videos(): Observable<Video[]> {
     return this.items;
   }
 
   constructor() {
-    this.items = [
+    const items = [
       {
         description: 'Description',
         title: 'Fast & Furious',
         image: '/assets/warsawjs-logo.png',
         liked: false,
-        rating: 4
+        rating: 5
       }, {
         description: 'Description',
         title: 'Slow & Lazy',
         image: '/assets/warsawjs-logo.png',
         liked: true,
-        rating: 5
+        rating: 2
       }
     ];
+
+    this.items = new BehaviorSubject<Video[]>([]);
+    this.items.next(items);
+
+    setTimeout(() => {
+      this.items.next([...items, {
+        rating: 4,
+        liked: true,
+        description: 'Description',
+        title: 'Moderate & Active',
+        image: '/assets/warsawjs-logo.png'
+      }]);
+    }, 5000);
   }
 
 }
