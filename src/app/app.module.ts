eed002fde6ae0f7002818084af5c0f9c021fbb1c
Mateusz Kocz <mateusz@kocz.pl>
2017-03-04 11:45:30 +0100

    Create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule, MdButtonModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppHeaderComponent} from './app-header/app-header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { VideosListComponent } from './videos-list/videos-list.component';
+import { VideoItemComponent } from './video-item/video-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    SearchBarComponent,
+    VideosListComponent,
+    VideoItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
